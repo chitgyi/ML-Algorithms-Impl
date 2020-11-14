4348bc52c91b0afe1fca156a7142f712985b1933
Chit Ye Aung <chityeaung.tu@gmail.com>
2020-11-14 12:52:35 +0630

    Add Func: sigmod

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.special import expit
 
 
 def mean_square_error(expected, predictons):
@@ -11,3 +12,7 @@ def euclidian_dis(a, b):
 
 def accuracy(expected, predictions):
     return np.sum(expected == predictions) / len(expected)
+
+
+def sigmod(x):
+    return 1 / (1 + np.exp(-x))
